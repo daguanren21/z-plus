fde8ca0d6078c886019dac8a9df12baa92cd7be4
daguanren21 <1363917353@qq.com>
2025-08-04 01:12:39 +0800

    feat: playground

--- apps/playground/src/App.vue
+++ apps/playground/src/App.vue
@@ -1,9 +1,12 @@
 <script setup lang="ts">
+import { ZMessage } from '@daguanren21/z-plus';
 import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <HelloWorld  />
+  <ZMessage >
+    <HelloWorld  />
+  </ZMessage>
 </template>
 
 
--- apps/playground/src/components/HelloWorld.vue
+++ apps/playground/src/components/HelloWorld.vue
@@ -1,9 +1,24 @@
 <script setup lang="ts">
-import { ZWatermark } from '@daguanren21/z-plus'
+import { ZWatermark,useMessage } from '@daguanren21/z-plus'
+import { ref } from 'vue'
+const message = useMessage()
+const isShow = ref(true)
+function add(){
+  isShow.value = false
+  message.add({
+    content: 'Hello, this is a message!',
+    duration: 3000,
+    position: 'top',
+    id: Date.now(),
+  })
+}
 </script>
 
 <template>
+  <div>
+ <button @click="add">测试测试</button>
   <ZWatermark
+  v-if="isShow"
     :content="['测试水印', 'zhangqihui']"
   >
     <div style="height: 800px;">
@@ -16,4 +31,6 @@ import { ZWatermark } from '@daguanren21/z-plus'
       <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quos quod deserunt quidem quas in rem ipsam ut nesciunt asperiores dignissimos recusandae minus, eaque, harum exercitationem esse sapiente? Eveniet, id provident!</p>
     </div>
   </ZWatermark>
+  </div>
+ 
 </template>
